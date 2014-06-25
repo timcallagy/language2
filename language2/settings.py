515f1963861732b7ff2a38779624368a12d2978b
timcallagy <timcallagy@gmail.com>
2014-06-25 08:35:55 +0100

    Commented out url config in settings.py.

--- language2/settings.py
+++ language2/settings.py
@@ -156,7 +156,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
 )
 
-ROOT_URLCONF = 'language2.urls'
+#ROOT_URLCONF = 'language2.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'language2.wsgi.application'
